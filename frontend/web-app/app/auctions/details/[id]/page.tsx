ac2b6df19900b1af1a278210951a8c48db7ee80f
Xxuu9 <tengxxu@gmail.com>
2024-12-21 20:27:31 -0500

    Add the notifications to the client

--- frontend/web-app/app/auctions/details/[id]/page.tsx
+++ frontend/web-app/app/auctions/details/[id]/page.tsx
@@ -1,4 +1,7 @@
-import { getDetailedViewData } from "@/app/actions/auctionActions";
+import {
+  getBidsForAuction,
+  getDetailedViewData,
+} from "@/app/actions/auctionActions";
 import Heading from "@/app/components/Heading";
 import React from "react";
 import CountdownTimer from "../../CountdownTimer";
@@ -7,10 +10,13 @@ import DetailedSpecs from "./DetailedSpecs";
 import { getCurrentUser } from "@/app/actions/authActions";
 import EditButton from "./EditButton";
 import DeleteButton from "./DeleteButton";
+import BidItem from "./BidItem";
+import BidList from "./BidList";
 
 export default async function Details({ params }: { params: { id: string } }) {
   const data = await getDetailedViewData(params.id);
   const user = await getCurrentUser();
+
   return (
     <div>
       <div className="flex justify-between">
@@ -32,9 +38,7 @@ export default async function Details({ params }: { params: { id: string } }) {
         <div className="w-full bg-gray-200 relative aspect-[4/3] rounded-lg overflow-hidden">
           <CarImage imageUrl={data.imageUrl} />
         </div>
-        <div className="border-2 rounded-lg p-2 bg-gray-100">
-          <Heading title="Bids" subtitle={""} />
-        </div>
+        <BidList user={user} auction={data} />
       </div>
       <div className="mt-3 grid grid-cols-1 rounded-lg">
         <DetailedSpecs auction={data} />
